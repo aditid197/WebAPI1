ce3b16ba0562c13dbbe0e05ede92b16879cafa6c
aditid197 <88667824+aditid197@users.noreply.github.com>
2021-08-10 00:55:19 +0530

    WebAPI

---
+++ WebAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EmployeeDataAccess;
+
+namespace WebAPI.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        public IEnumerable<Employee> GetAllEmployees()
+        {
+            using(EmployeeEntities entities = new EmployeeEntities())
+            {
+                return entities.Employees.ToList();
+            }
+        }
+
+        public Employee GetEmployeeDetails(int id)
+        {
+            using (EmployeeEntities entities = new EmployeeEntities())
+            {
+                return entities.Employees.FirstOrDefault(e => e.EmployeeId == id);
+            }
+        }
+    }
+}
